a6c6b189f33880808c71e1adc7f99a8cbb2e8136
Regan-John Daniels <reganjohndaniels@gmail.com>
2022-08-03 00:00:02 +0200

    updates

--- ProjectA_ElectronicsStore_TransactionalData.py
+++ ProjectA_ElectronicsStore_TransactionalData.py
@@ -21,7 +21,7 @@ def date_cleaning(data, correct_date_format):
     bad_dates = data[~data['date'].str.contains(r'^\d{4}')]
     good_dates = data[data['date'].str.contains(r'^\d{4}')]
 
-    remove = ['-', ' ']
+    remove = ['-', ' ', '*']
     for i in remove:
         bad_dates['date'] = [x.replace(i, '/') for x in bad_dates['date']]
 
@@ -34,9 +34,10 @@ def date_cleaning(data, correct_date_format):
 
         logging.warning('All faulty date formatting was not identified. Check output of error_dates.csv for the faulty date formats.')
         error_dates.to_csv(f'{OUTPUT_DIR}error_dates.csv')
-    finally:
+
         bad_dates['date'] = [datetime.strptime(x, '%m/%d/%Y') for x in bad_dates['date']]
         bad_dates['date'] = [datetime.strftime(x, correct_date_format) for x in bad_dates['date']]
+
 
     output = pd.concat([bad_dates, good_dates])
     return output
@@ -45,4 +46,4 @@ def date_cleaning(data, correct_date_format):
 # df['shopping_cart'] = df['shopping_cart'].apply(lambda x: eval(x))
 
 df = pd.read_csv(f'{OUTPUT_DIR}dirty_data_test.csv')
-date_cleaning(df, '%Y-%m-%d').to_csv(f'{OUTPUT_DIR}test_output.csv', index=False)
+date_cleaning(df, '%Y/%m/%d').to_csv(f'{OUTPUT_DIR}test_output.csv', index=False)
